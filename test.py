93c0a0e8032fe8c389e621ce0df1848a806238b9
Moomen123Msaadi <158359386+Moomen123Msaadi@users.noreply.github.com>
2024-04-11 00:18:21 +0200

    -Corrected some tests to include the addition of the progress bar percentage. -Added tests related to the mark_unDone() function

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from unittest.mock import patch
-from Task_List import addTasks, tasks, completeTask, checkTask, deleteTask, random_task
+from Task_List import addTasks, tasks, completeTask, checkTask, deleteTask, random_task, mark_unDone
 
 
 def test_addTasks(capsys):
@@ -41,10 +41,11 @@ def test_checkTask_with_tasks(capsys):
     checkTask()
     captured = capsys.readouterr()
 
-    assert captured.out.strip() == ("The Current Tasks are:\n"
-                                    "Task #1 : Task 1 \n"
-                                    "Task #2 : Task 2 \n"
-                                    "Task #3 : Task 3")
+    assert captured.out.strip() == ('The Current Tasks are:\n'
+                                    'Task #1 : Task 1 \n'
+                                    'Task #2 : Task 2 \n'
+                                    'Task #3 : Task 3 \n'
+                                    'Tasks done: 0.0%')
 
 
 def test_checkTask_marked_asDone(capsys):
@@ -57,10 +58,11 @@ def test_checkTask_marked_asDone(capsys):
     })
     checkTask()
     captured = capsys.readouterr()
-    assert captured.out.strip() == (("The Current Tasks are:\n"
-                                     "Task #1 : Task 1 (done) \n"
-                                     "Task #2 : Task 2 \n"
-                                     "Task #3 : Task 3 (done)"))
+    assert captured.out.strip() == ("The Current Tasks are:\n"
+                                    "Task #1 : Task 1 (done) \n"
+                                    "Task #2 : Task 2 \n"
+                                    "Task #3 : Task 3 (done) \n"
+                                    "Tasks done: 66.66666666666666%")
 
 
 def test_completeTask_no_tasks(capsys):
@@ -150,6 +152,7 @@ def test_deleteTask_with_tasks(capsys):
             "Task #1 : Task1 \n"
             "Task #2 : Task2 \n"
             "Task #3 : Task3 \n"
+            "Tasks done: 0.0%\n"
             "The task #2 has been Deleted\n") == captured.out
 
 
@@ -171,6 +174,7 @@ def test_deleteTask_with_done_tasks(capsys):
             "Task #1 : Task1 (done) \n"
             "Task #2 : Task2 \n"
             "Task #3 : Task3 \n"
+            "Tasks done: 33.33333333333333%\n"
             "The task #1 has been Deleted\n") == captured.out
 
 
@@ -214,3 +218,75 @@ def test_random_task_with_tasks(capsys):
     assert (captured.out == f"Random Task: #1: Task1\n"
             or captured.out == f"Random Task: #2: Task2\n"
             or captured.out == f"Random Task: #3: Task3\n")
+
+
+def test_markUndone_no_tasks(capsys):
+    tasks.clear()
+
+    mark_unDone()
+
+    captured = capsys.readouterr()
+
+    assert (captured.out == f"The are no current tasks to be Unmarked\n")
+
+
+def test_markUndone_all_tasks_undone(capsys):
+    tasks.clear()
+
+    tasks.update({
+        1: {"task": "Task1", "done": False, "category": ""},
+        2: {"task": "Task2", "done": False, "category": ""},
+        3: {"task": "Task3", "done": False, "category": ""}
+    })
+
+    mark_unDone()
+
+    captured = capsys.readouterr()
+
+    assert (captured.out == f"All tasks are Unmarked.\n")
+
+
+def test_markUndone_all_tasks_done(capsys):
+    tasks.clear()
+
+    tasks.update({
+        1: {"task": "Task1", "done": True, "category": ""},
+        2: {"task": "Task2", "done": True, "category": ""},
+        3: {"task": "Task3", "done": True, "category": ""}
+    })
+
+    with patch('builtins.input', return_value='1'):
+        mark_unDone()
+
+    captured = capsys.readouterr()
+
+    assert (captured.out == f"Task #1 : Task1\n"
+                            "Task #2 : Task2\n"
+                            "Task #3 : Task3\n"
+                            "The task #1 (Task1) has been Unmarked\n")
+
+def test_markUndone_some_tasks(capsys):
+    tasks.clear()
+
+    tasks.update({
+        1: {"task": "Task1", "done": False, "category": ""},
+        2: {"task": "Task2", "done": True, "category": ""},
+        3: {"task": "Task3", "done": False, "category": ""}
+    })
+
+    with patch('builtins.input', return_value='1'):
+        mark_unDone()
+
+    captured1 = capsys.readouterr()
+
+    with patch('builtins.input', return_value='2'):
+        mark_unDone()
+
+    captured2 = capsys.readouterr()
+
+    assert (captured1.out == f"Task #2 : Task2\n"
+                             f"The task has yet to be Unmarked. Choose another task.\n")
+
+    assert (captured2.out == f"Task #2 : Task2\n"
+                             f"The task #2 (Task2) has been Unmarked\n")
+
